a4df0503acc00c333a51abb8180367e15e601e16
Evgeny Pimenov <evgeny-p-@users.noreply.github.com>
2017-03-04 17:43:05 +0700

    Extract printing array to function

--- main.c
+++ main.c
@@ -16,8 +16,9 @@ int min_element(int *arr, int size)
     return min;
 }
 
-int main()
+void print_array(int *arr, int size)
 {
+<<<<<<< HEAD
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
 
@@ -26,10 +27,23 @@ int main()
     printf("Array: ");
     int i;
     for (i = 0; i < 10; ++i) {
+=======
+    printf("Array: ");
+    int i;
+    for (i = 0; i < size; ++i) {
+>>>>>>> Extract printing array to function
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
 
+int main()
+{
+    int arr[] = {3, 1, 4, 1, 5, 9, 2};
+
+    const int min = min_element(arr, N_ELEMENTS(arr));
+
+    print_array(arr, N_ELEMENTS(arr));
     printf("Min element: %d\n", min);
 
     return 0;
